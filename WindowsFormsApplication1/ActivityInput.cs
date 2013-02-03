d2a5842aecc10ee5c0a738aaf6a5f8f2ad4b4fc4
Justin Bolling <justinbolling@gmail.com>
2013-02-03 09:57:57 -0500

    Fixed date picker formatting + More code cleanup - added code comments, removed some unnecessary methods

--- WindowsFormsApplication1/ActivityInput.cs
+++ WindowsFormsApplication1/ActivityInput.cs
@@ -76,13 +76,9 @@ namespace StudentActivityTracker
             }
         }
 
-        private void SetToParentSize(Control ctrl, Control parent)
-        {
-            ctrl.Top = parent.Top;
-            ctrl.Left = parent.Left;
-            ctrl.Size = parent.Size;
-        }
-
+        /**
+         * Creates the initial buttons and sets their data.
+         */
         private void InitializeBoard()
         {
             int studentCount = ds1.Tables["Students"].Rows.Count;
@@ -154,18 +150,7 @@ namespace StudentActivityTracker
                 tp1.Controls.Add(studentGroup);
             }
             bg.TabPages.Add(tp1);
-
-            TabPage controlPage = new TabPage();
-            Button saveButton = new Button();
-            saveButton.Text = "Save";
-            controlPage.Controls.Add(saveButton);
-            saveButton.Click += new EventHandler(this.SaveBtn_Click);
-
-            DateTimePicker controlDate = new DateTimePicker();
-            controlDate.Value = curDate;
-            controlPage.Controls.Add(controlDate);
-            controlDate.ValueChanged += new EventHandler(this.ControlDate_ValueChanged);
-            bg.TabPages.Add(controlPage);
+            CreateControlPage(ref bg);
             this.Controls.Add(bg);
         }
 
@@ -266,15 +251,19 @@ namespace StudentActivityTracker
                     newButton.Width = nameGroup.Width - 8;
                 }
             }
+            FormatControlPage(bg);
+        }
 
-            /**TODO:  SET THE CONTROL DATE POSITIOn
-            DateTimePicker controlDate = new DateTimePicker();
-            controlDate.Value = curDate;
+        /**
+         * Place the control page controls in the correct spot
+         */
+        private void FormatControlPage(TabControl bg)
+        {
+            TabPage controlPage = (TabPage)bg.Controls.Find("controlPage", false)[0];
+            Button saveButton = (Button)controlPage.Controls.Find("controlSave", false)[0];
+            DateTimePicker controlDate = (DateTimePicker)controlPage.Controls.Find("controlDate", false)[0];
             controlDate.Left = saveButton.Left;
             controlDate.Top = saveButton.Bottom + 5;
-             */
-
-
         }
 
         /**
@@ -310,186 +299,27 @@ namespace StudentActivityTracker
         }
 
         /**
-         * TODO::Needs to be removed
+         * Creates the controls for the control page
          */
-        private void DisplayActivities()
+        private void CreateControlPage(ref TabControl bg)
         {
-            int windowWidth = Control.FromHandle(GetForegroundWindow()).Width;
-            int windowHeight = Control.FromHandle(GetForegroundWindow()).Height;
-            int vPadding = (int)(windowHeight / 60.0);
-            int hPadding = (int)(windowWidth / 80.0);
-            int hSize = (int)(windowWidth / 5.4);
-            int vSize = (int)(windowHeight / 1.45);
-            int studentCount = ds1.Tables["Students"].Rows.Count;
-            int activityCount = ds1.Tables["Activities"].Rows.Count;
-            int buttonHeight = (int)(vSize - vSize / 4 - 5 * 1.0) / activityCount;
-            Font buttonFont = new Font("Arial", buttonHeight / 3);
-            Font navFont = new Font("Arial", buttonHeight / 6);
-
-            int strLength = MAX_ACTIVITY_LEN < 8 ? 8 : MAX_ACTIVITY_LEN;
-            String tempSize = "";
-            for (int i = 0; i < strLength; i++)
-            {
-                tempSize = tempSize + "A";
-            }
-            float prevBFontSize = buttonFont.Size;
-            SizeF sizeB;
-            using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1)))
-            {
-                sizeB = graphics.MeasureString(tempSize, buttonFont);
-            }
-
-            float desiredBSize = buttonHeight - 5;
-            float maxBFontHeight = (buttonFont.Size * desiredBSize) / sizeB.Height;
-            float maxBFontWidth = (buttonFont.Size * (hSize - 15)) / sizeB.Width;
-            float newBFontSize = maxBFontHeight < maxBFontWidth ? maxBFontHeight : maxBFontWidth;
-            buttonFont = new Font("Arial", newBFontSize);
-            navFont = new Font("Arial", newBFontSize);
-
-            Point listOffset = new Point(0,5);
-
-            DataRow dr = ds1.Tables["Students"].Rows[0];
-            TabControl bg = new TabControl();
-            bg.Name = "backgroundPage";
-            TabPage tp1 = new TabPage();
-            tp1.Name = "studentPage0";
-            tp1.TabIndex = 0;
-            //tp1.BackColor = Color.GhostWhite;
-
-            bg.Height = windowHeight;
-            bg.Width = windowWidth;
-
-            Point topLeft = new Point(4, 8);
-
-            bg.Location = new Point(0, 0);
-
-            DataRow dr2 = ds1.Tables["Students"].Rows[0];
-            for (int i = 0; i < studentCount; i++)
-            {
-                if (i > 0 && i % PAGE_TOTAL == 0)
-                {
-                    Button pageNext = new Button();
-                    pageNext.Location = new Point(hSize * 4 + hSize / 2 + hPadding * 5, (int)(vSize + (windowHeight-vSize)/2.5 + vPadding * 2));
-                    pageNext.Width = (int)(hSize / 2.0);
-                    pageNext.Height = (int)(buttonHeight - 5);
-                    pageNext.Text = "Next";
-                    pageNext.Click += new EventHandler(this.NextBtn_Click);
-                    pageNext.Font = navFont;
-                    tp1.Controls.Add(pageNext);
-
-                    bg.Controls.Add(tp1);
-
-                    tp1 = new TabPage();
-                    Button pagePrev = new Button();
-                    pagePrev.Location = new Point(hSize * 4 + hPadding * 5, (int)(vSize + (windowHeight - vSize) / 2.5 + vPadding * 2));
-                    pagePrev.Width = (int)(hSize / 2.0);
-                    pagePrev.Height = (int)(buttonHeight - 5);
-                    pagePrev.Text = "Previous";
-                    pagePrev.Click += new EventHandler(this.PrevBtn_Click);
-                    pagePrev.Font = navFont;
-                    tp1.Controls.Add(pagePrev);
-                    tp1.Name = "studentPage" + i / PAGE_TOTAL;
-                }
-                //Point curLoc = new Point((int)(((i%10)%5+1)*hPadding +((i%10)%5) * hSize), 
-                    //(int)((((i%10)/5)+1)*vPadding + ((i%10)/5)*vSize));
-                Point curLoc = new Point((int)((i%5+1)*hPadding +(i%5) * hSize), 
-                      (int)((windowHeight - vSize)/2.5));
-
-                GroupBox studentGroup = new GroupBox();
-                //studentGroup.BackColor = Color.Gainsboro;
-                studentGroup.Padding = DefaultPadding;
-                studentGroup.Height = vSize;
-                studentGroup.Width = hSize;
-                studentGroup.Location = curLoc;
-                studentGroup.Name = ""+i;
-                //studentGroup.Paint += new PaintEventHandler(panel1_Paint);
-                //studentGroup.BackgroundImage = bmp;
-                //studentGroup.ForeColor = Color.Aqua;
-
-                GroupBox nameGroup = new GroupBox();
-                nameGroup.Width = hSize-7;
-                nameGroup.Height = vSize/4;
-                nameGroup.Location = topLeft;
-                //nameGroup.BackColor = Color.GhostWhite;
-
-                Label studentFirstName = new Label();
-                Label studentLastName = new Label();
-
-                studentFirstName.Text = ds1.Tables["Students"].Rows[i].ItemArray.GetValue(1).ToString();
-                studentLastName.Text = ds1.Tables["Students"].Rows[i].ItemArray.GetValue(2).ToString();
-                float prevFontSize = studentFirstName.Font.Size;
-                SizeF sizeFirst, sizeLast;
-                using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1)))
-                {
-                    sizeFirst = graphics.MeasureString(studentFirstName.Text, studentFirstName.Font );
-                    sizeLast = graphics.MeasureString(studentLastName.Text, studentLastName.Font );
-                }
-
-                float desiredSize = nameGroup.Height/(float)(3.0);
-                float maxFontHeight = (studentFirstName.Font.Size * desiredSize) / sizeFirst.Height;
-                float maxFontWidth = sizeFirst.Width < sizeLast.Width ? (studentLastName.Font.Size * (nameGroup.Width - 5)) / sizeLast.Width
-                    : (studentFirstName.Font.Size * (nameGroup.Width - 5)) / sizeFirst.Width;
-                float newFontSize = maxFontHeight < maxFontWidth ? maxFontHeight : maxFontWidth;
-                Font nameFont = new Font("Arial", newFontSize);
-
-                studentFirstName.AutoSize = true;
-                studentLastName.AutoSize = true;
-
-                studentFirstName.Font = nameFont;
-                studentLastName.Font = nameFont;
-
-                using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1)))
-                {
-                    sizeFirst = graphics.MeasureString(studentFirstName.Text, studentFirstName.Font );
-                    sizeLast = graphics.MeasureString(studentLastName.Text, studentLastName.Font );
-                }
-
-                studentFirstName.Location = new Point((int)(studentGroup.Width - sizeFirst.Width) / 2, nameGroup.Height / 6);
-                studentLastName.Location = new Point((int)(studentGroup.Width - sizeLast.Width) / 2, (int)(nameGroup.Height - sizeLast.Height - nameGroup.Height/6 ));
-
-                nameGroup.Controls.Add(studentFirstName);
-                nameGroup.Controls.Add(studentLastName);
-
-                studentGroup.Controls.Add(nameGroup);
-
-                CreateButtonBackgrounds(nameGroup.Width - 8,buttonHeight - 5 );
-                for (int j = 0; j < activityCount; j++)
-                {
-                    CheckBox newButton = new CheckBox();
-                    newButton.Appearance = Appearance.Button;
-                    newButton.Text = ds1.Tables["Activities"].Rows[j].ItemArray.GetValue(0).ToString();
-                    newButton.Font = buttonFont;
-                    newButton.Location = new Point (nameGroup.Left + 5, nameGroup.Bottom + 5 + j*buttonHeight);
-                    newButton.Height = buttonHeight - 5;
-                    newButton.Width = nameGroup.Width - 8;
-                    newButton.TextAlign = ContentAlignment.MiddleCenter;
-                    newButton.Click += new EventHandler(this.ActivityBtn_Click);
-                    studentGroup.Controls.Add(newButton);
-                }
-                tp1.Controls.Add(studentGroup);
-            }
-            bg.TabPages.Add(tp1);
-
             TabPage controlPage = new TabPage();
+            controlPage.Name = "controlPage";
             Button saveButton = new Button();
+            saveButton.Name = "controlSave";
             saveButton.Text = "Save";
             controlPage.Controls.Add(saveButton);
             saveButton.Click += new EventHandler(this.SaveBtn_Click);
 
             DateTimePicker controlDate = new DateTimePicker();
+            controlDate.Name = "controlDate";
             controlDate.Value = curDate;
             controlDate.Left = saveButton.Left;
             controlDate.Top = saveButton.Bottom + 5;
             controlPage.Controls.Add(controlDate);
             controlDate.ValueChanged += new EventHandler(this.ControlDate_ValueChanged);
 
-
             bg.TabPages.Add(controlPage);
-
-
-
-
-            this.Controls.Add(bg);
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -500,11 +330,20 @@ namespace StudentActivityTracker
             ControlPaint.DrawBorder3D(e.Graphics, panel1.ClientRectangle, Border3DStyle.Raised, Border3DSide.All);
         }
 
+        /**
+         * Triggers when the form is resized to redraw the board
+         */
         private void Form_Resize(object sender, System.EventArgs e)
         {
             DrawBoard();
         }
 
+
+        /**
+         *Drawing to create the button backgrounds when clicked...
+         *
+         * TODO::Can probably be cleaned up after activity number is added to db
+         */
         void CreateButtonBackgrounds(int height, int width)
         {
             bgList.Clear();
@@ -554,6 +393,11 @@ namespace StudentActivityTracker
             bgList.Add(bmp);
         }
 
+
+        /**
+         * Method to determine which color the background should be 
+         * based on whether it was the first or second clicked item
+         */
         Bitmap GetBackgroundForClick(String number = "0")
         {
             int numVal;
@@ -699,19 +543,26 @@ namespace StudentActivityTracker
             }
         }
 
+        /**
+         * Called when Form is closed, only purpose is to save changes to DB
+         */
         protected override void OnFormClosing(FormClosingEventArgs e)
         {
             SaveChanges();
             base.OnFormClosing(e);
         }
 
+        /**
+         * Goes through all the controls and saves the changed items to the db..
+         * 
+         * TODO: Can be cleaned up with the "Find" method and can probably be combined with
+         * LoadDate to remove duplicated code
+         */
         void SaveChanges()
         {
 
             System.Data.OleDb.OleDbConnection conn;
             conn = new System.Data.OleDb.OleDbConnection();
-            //conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
-                //@"Data source= C:\Users\Justin\Documents\DailyFive.mdb;";
             conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
                 @"Data source= DailyFive.mdb;";
 
@@ -780,6 +631,9 @@ namespace StudentActivityTracker
             }
         }
 
+        /**Whenever the date field on the control page changes values, call
+         * this method which saves the changes and loads a new date
+         */
         private void ControlDate_ValueChanged(object sender, EventArgs e)
         {
             SaveChanges();
@@ -788,22 +642,30 @@ namespace StudentActivityTracker
             LoadDate();
         }
 
+        /**
+         * Method called when the form loads where we initialize and
+         * do an initial drawing
+         */
         void Page_Load(object sender, EventArgs e)
         {
-            // Write status to file.
-            //DisplayActivities();
             InitializeBoard();
             DrawBoard();
             LoadDate();
         }
 
+
+        /**
+         * handler for when any of the activity buttons are clicked
+         * 
+         * Can definitely be re-written to use 'find'and seems to share 
+         * a lot of common code with LoadDate as well.  Probably the most important of
+         * methods to re-write...
+         */
         void ActivityBtn_Click(Object sender,
                            EventArgs e)
         {
             System.Data.OleDb.OleDbConnection conn;
             conn = new System.Data.OleDb.OleDbConnection();
-            //conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
-            //@"Data source= C:\Users\Justin\Documents\DailyFive.mdb;";
             conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
                 @"Data source= DailyFive.mdb;";
 
@@ -956,12 +818,21 @@ namespace StudentActivityTracker
 
         }
 
+        /**
+         * Simple handler that just saves whenever the save button is clicked
+         * on the control page
+         */
         void SaveBtn_Click(Object sender,
            EventArgs e)
         {
             SaveChanges();
         }
 
+        /**
+         *Simple handler that goes to the next page whenever the next button is clicked
+         *Works on the assumption there isn't going to be a 'next' button on a page that doesn't
+         *actually have a 'next' page
+         */
         void NextBtn_Click(Object sender,
                    EventArgs e)
         {
@@ -969,6 +840,11 @@ namespace StudentActivityTracker
             backgroundPage.SelectedIndex = backgroundPage.SelectedIndex + 1;
         }
 
+        /**
+         * *Simple handler that goes to the prev page whenever the prev button is clicked
+         * Works on the assumption there isn't going to be a 'prev' button on a page that doesn't
+         * actually have a 'prev' page
+         */
         void PrevBtn_Click(Object sender,
            EventArgs e)
         {
@@ -1014,6 +890,7 @@ namespace StudentActivityTracker
         }
     }
 
+    //TODO::This code is open source and can probably be removed once resize is implemented correctly
     /// <summary>
     /// Class used to preserve / restore state of the form
     /// </summary>
